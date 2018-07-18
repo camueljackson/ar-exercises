97259b73672fa732c8c8e70ef6aa5bd9237b1a83
camueljackson <camille.a.pelletier@gmail.com>
2018-07-18 00:37:23 +0000

    ex 1-6 complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,22 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+
+
+for elem in @mens_stores
+  puts elem.name
+  puts elem.annual_revenue
+end
+
+puts "--------- ******** ---------"
+
+for wstore in @womens_stores
+  puts wstore.name
+  puts wstore.annual_revenue
+end
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,13 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+
+@store4 = Store.find_by(id: 4)
+
+
+  @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+  @store1.employees.create(first_name: "Damo", last_name: "Lex", hourly_rate: 75)
+  @store2.employees.create(first_name: "Cam", last_name: "Pelletier", hourly_rate: 100)
+  @store2.employees.create(first_name: "Quentin", last_name: "Luisi", hourly_rate: 120)
+  @store4.employees.create(first_name: "Michael", last_name: "Mansouri", hourly_rate: 70)
+  @store4.employees.create(first_name: "Kay", last_name: "Neelin", hourly_rate: 80)
